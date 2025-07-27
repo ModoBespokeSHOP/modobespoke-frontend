933356e6e13b82e46b7fc6cff4f795ba00f85e80
Modo Bespoke Tech <modobespoketech@outlook.com>
2025-07-27 03:30:08 +0300

    replace

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/service.php",
-        destination: "/api/service", // Перенаправляем все запросы /service.php → pages/api/service.js
-      },
-    ];
   },
 };
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,10 +1,11 @@
-import Head from "next/head";
+// pages/_app.js
+import Head from "next/head"; // ← добавили Head
+import "../styles/globals.css";
 import Script from "next/script";
 import Link from "next/link";
 import Footer from "../components/Footer";
 import { CartProvider } from "../context/CartContext";
 import { ToastProvider } from "../context/ToastContext";
-import "../styles/globals.css";
 
 function Header() {
   return (
@@ -25,19 +26,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <ToastProvider>
       <CartProvider>
-        <Script
-          type="text/javascript"
-          src="https://cdn.jsdelivr.net/npm/@cdek-it/widget@2"
-          charSet="utf-8"
-          strategy="afterInteractive" // Это подходит для виджета
-        />
-        <Script
-          src="https://yookassa.ru/v3/yookassa.js"
-          strategy="afterInteractive"
-        />
-        <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-        </Head>
+        <Script src="https://js.yookassa.ru/v3" strategy="afterInteractive" />
         <Header />
         <Component {...pageProps} />
         <Footer />
